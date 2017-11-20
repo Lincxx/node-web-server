c5402034723f2781831443af3e1b6e83f096a97a
Jeremy Lincoln <bossco28@gmail.com>
2017-11-20 14:45:50 -0500

    Add in two more routes

--- server.js
+++ server.js
@@ -5,5 +5,23 @@ var app = express();
 
 
 app.get('/', (req, res) => {
-    
-})
+    //res.send('<h1>Hello Express</h1>');
+    res.send({
+        name:'Jeremy',
+        age:40,
+        likes:['sleeping', 'relaxing']
+    })
+});
+
+app.get('/about', (req, res)=>{
+    res.send('About Page');
+});
+app.get('/bad', (req, res) =>{
+    res.send({
+        error:'Unable to fulfill this request'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server up and running');
+});
