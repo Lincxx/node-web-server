d789869bb56926c03f974435a4c503035913ba5f
Jeremy Lincoln <bossco28@gmail.com>
2017-11-21 08:03:51 -0500

    Add in hbs registerHelper

--- server.js
+++ server.js
@@ -11,19 +11,24 @@ app.set('view engine', 'hbs');
 //middleware -
 app.use(express.static(__dirname + '/public'));
 
+//This is a hbs helper function and it will be looked for first
+//We are using this because we were passing the currentYear twice
+//takes 2 parameter - first the name of the helper and then the function
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
 app.get('/', (req, res) => {
     //res.send('<h1>Hello Express</h1>');
     res.render('home', {
         welcomeMessage: 'Welcome to the home page',
-        pageTitle: 'Home Page',
-        currentYear: new Date().getFullYear()
+        pageTitle: 'Home Page'
     });
 });
 
 app.get('/about', (req, res)=> {
     res.render('about', {
-        pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
+        pageTitle: 'About Page'
     });
 });
 
