2204d94656bd212a82125d3424f35097345a7d92
Jeremy Lincoln <bossco28@gmail.com>
2017-11-21 08:56:39 -0500

    Add new middleware for a logger

--- server.js
+++ server.js
@@ -1,27 +1,57 @@
 const express = require('express');
 const hbs     = require('hbs');
+const fs      = require('fs');
 
 //new express app
 var app = express();
 
-//exppress configurations 
+//////////////
+//express configurations
+/////////////
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
-//middleware -
+//////////////
+//Middleware
+/////////////
 app.use(express.static(__dirname + '/public'));
+//an empty middleware will cause your app not to run 
+app.use((req, res, next) => {
+    //logger
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append to sever.log')
+        }
+    });
+    next();
+});
 
-//This is a hbs helper function and it will be looked for first
+
+//////////////
+//Helpers
+/////////////
+//This is a hbs register helper function and it will be looked for first
+//hbs register helper can take arguments
 //We are using this because we were passing the currentYear twice
 //takes 2 parameter - first the name of the helper and then the function
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
 });
 
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+//////////////
+//Routes
+/////////////
 app.get('/', (req, res) => {
     //res.send('<h1>Hello Express</h1>');
     res.render('home', {
-        welcomeMessage: 'Welcome to the home page',
+        welcomeMessage: 'Welcome to my website',
         pageTitle: 'Home Page'
     });
 });
@@ -38,6 +68,10 @@ app.get('/bad', (req, res) =>{
     });
 });
 
+
+//////////////
+//Server
+/////////////
 app.listen(3000, () => {
     console.log('Server up and running');
 });
